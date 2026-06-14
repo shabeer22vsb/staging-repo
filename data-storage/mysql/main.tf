4bd671a6d036b70c6151850b7dc210dc19fdcaa1
shabeer22vsb <shabeer22vsb@gmail.com>
2026-06-14 11:53:38 +0100

    updated data sources

--- data-storage/mysql/main.tf
+++ data-storage/mysql/main.tf
@@ -1,5 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.50"
+    }
+  }
+  backend "s3" {
+    bucket       = "my-tf-practical-bucket-166373406634-eu-west-1-an"
+    key          = "stage/data-stores/mysql/terraform.tfstate"
+    use_lockfile = true
+    region       = "eu-west-1"
+  }
+}
+provider "aws" {
+  region = "eu-west-1"           # ← default provider (no alias)
+}
 provider "aws" {
     region = "eu-west-1"
+    alias = "ireland"
+}
+provider "aws" {
+    region = "eu-west-2"
+    alias = "london"
 }
 data "aws_secretsmanager_secret_version" "db-creds" {
   secret_id = "db-creds"
@@ -9,19 +31,25 @@ locals {
     data.aws_secretsmanager_secret_version.db-creds.secret_string
   )
 }
-module "data-storage"{
-  source = "../../../modules/services/data-storage"
+module "data-storage-ireland"{
+  source = "github.com/shabeer22vsb/modules//services/data-storage"
   db_password = local.db_creds.password
   db_username = local.db_creds.username
   db_instance_class = var.db_instance_class
   allocated_storage = var.allocated_storage
   skip_final_snapshot = var.skip_final_snapshot
+  providers = {
+    aws = aws.ireland
+  }
 }
-terraform {
-  backend "s3" {
-    bucket       = "my-tf-practical-bucket-166373406634-eu-west-1-an"
-    key          = "stage/data-stores/mysql/terraform.tfstate"
-    use_lockfile = true
-    region       = "eu-west-1"
+module "data-storage-london"{
+  source = "github.com/shabeer22vsb/modules//services/data-storage"
+  db_password = local.db_creds.password
+  db_username = local.db_creds.username
+  db_instance_class = var.db_instance_class
+  allocated_storage = var.allocated_storage
+  skip_final_snapshot = var.skip_final_snapshot
+  providers = {
+    aws = aws.london
   }
 }
--- data-storage/mysql/outputs.tf
+++ data-storage/mysql/outputs.tf
@@ -1,8 +1,8 @@
 output "address" {
     description = "connect to DB using this endpoint"
-    value = module.data-storage.address
+    value = module.data-storage-ireland.address
 }
 output "port" {
     description = "connect  port using this endpoint"
-    value = module.data-storage.port
+    value = module.data-storage-ireland.port
 }
